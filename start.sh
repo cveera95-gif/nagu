988895031bd5d60a2a63c3f993720fc40c1ecbd9
root <root@ip-172-31-38-31.ap-south-1.compute.internal>
2026-02-14 05:08:48 +0000

    deleting the commit messages

--- start.sh
+++
@@ -1,8 +0,0 @@
-rm -rf .git
-git init
-git add .
-git commit -m "Fresh start"   # 👈 MUST come before push
-git branch -M main
-git remote add origin https://github.com/cveera95-gif/nagu.git
-git push -f origin main
-
